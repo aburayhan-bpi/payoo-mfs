db61dfde219c14e9cb05b944a9025c540783f7ca
aburayhan-bpi <aburayhan.bpi@gmail.com>
2024-09-19 19:42:34 +0600

    added add money functionality using JavaScript

---
+++ js/home.js
@@ -0,0 +1,24 @@
+const addMoneyBtn = document.getElementById('addMoneyBtn');
+addMoneyBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const amountInput = document.getElementById('amountInput').value;
+    // console.log(amountInput);
+
+    const pinInput = document.getElementById('pinInput').value;
+    // console.log(pinInput);
+    if (pinInput === '1234') {
+        // console.log('adding money');
+        const balance = document.getElementById('accountBalance').innerText;
+        // console.log(Number(balance));
+
+        const amountInputNumber = Number(amountInput);
+        const balanceNumber = Number(balance);
+        const newBalance = (amountInputNumber + balanceNumber);
+        // console.log(newBalance);
+        document.getElementById('accountBalance').innerText = newBalance;
+    }
+    else {
+        alert('Failed to add money. Enter your correct pin.');
+    }
+})  
--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
 // set handler 
 document.getElementById('loginBtn').addEventListener('click', function (event) {
     event.preventDefault();
-    const number = document.getElementById('phoneNumber').value
-    console.log(number);
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const pinNumber = document.getElementById('pinNumber').value;
+
+    if (phoneNumber === '5' && pinNumber === '2233') {
+        console.log('you are logged in');
+        window.location.href = '/home.html'
+    }
+    else {
+        alert('Please submit correct phone number or pin');
+    }
+
 })
